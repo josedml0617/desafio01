a1b1b2a6728f19bdda469b0406995d5ac90f3807
José Marcos <josemarcosdemelolima1@gmail.com>
2026-02-11 07:50:51 -0300

    Create ShippingService

--- src/main/java/com/devsuperior/desafio01/Desafio01Application.java
+++ src/main/java/com/devsuperior/desafio01/Desafio01Application.java
@@ -1,13 +1,29 @@
 package com.devsuperior.desafio01;
 
+import java.util.Locale;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.desafio01.entities.Order;
+import com.devsuperior.desafio01.services.ShippingService;
+
 @SpringBootApplication
-public class Desafio01Application {
+public class Desafio01Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio01Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Locale.setDefault(Locale.US);
+		
+		Order order = new Order(5533, 120.00, 10.);
+		ShippingService ss = new ShippingService();
+		System.out.println("Valor do frete: "+String.format("%.2f",ss.shipment(order)));
+		
 	}
 
 }
